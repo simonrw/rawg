86e5ffcd409bf0c29b1176b9a56671d06e573ea6
Simon Walker <s.r.walker101@googlemail.com>
2018-10-27 11:28:56 +0100

    Include flag and yaml packages

--- main.go
+++ main.go
@@ -2,11 +2,34 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
 
 	. "github.com/dave/jennifer/jen"
+	flag "github.com/spf13/pflag"
+	yaml "gopkg.in/yaml.v2"
 )
 
+type Definition struct {
+}
+
 func main() {
+	var port *int = flag.IntP("port", "p", 8080, "Port to listen on")
+	flag.Parse()
+
+	_ = port
+
+	bytes, err := ioutil.ReadFile("definition.yml")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var def Definition
+	err = yaml.Unmarshal(bytes, &def)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	f := NewFile("main")
 	f.Func().Id("main").Params().Block(
 		Qual("fmt", "Println").Call(Lit("Hello world")),
