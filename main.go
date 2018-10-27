537f9484509e85f4925b4ed05c403f8d93c62c38
Simon Walker <s.r.walker101@googlemail.com>
2018-10-27 22:45:19 +0100

    Add example code to main.go

--- main.go
+++ main.go
@@ -1,4 +1,27 @@
 package main
 
+import "github.com/dave/jennifer/jen"
+
 func main() {
+	file := jen.NewFile("main")
+	rootURL := "https://example.com"
+	CreateClient(file, rootURL)
+
+	apiTypes := ApiTypes{
+		ApiType{
+			Name: "Record",
+			Parameters: TypeParameters{
+				TypeParameter{
+					Name: "foo",
+					Type: "int",
+				},
+			},
+		},
+	}
+	err := GenerateTypes(file, apiTypes)
+	if err != nil {
+		panic(err)
+	}
+
+	file.Save("/tmp/out.go")
 }
