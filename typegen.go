cfcba436878d17bb927aacbe2999dab4ca8688f7
Simon Walker <s.r.walker101@googlemail.com>
2018-10-27 21:04:08 +0100

    Add some helpful comments

--- typegen.go
+++ typegen.go
@@ -13,9 +13,12 @@ func GenerateTypes(types ApiTypes) (string, error) {
 	// Writer for final result
 	var b strings.Builder
 
+	// Iterate over the types found in the definition
 	for typeIdx, typedef := range types {
 		argDefs := make([]jen.Code, len(typedef.Parameters))
 
+		// Iterate over the parameter definitions
+		// TODO: handle arrays and maps
 		for paramIdx, param := range typedef.Parameters {
 			switch param.Type {
 			case "int":
@@ -29,9 +32,16 @@ func GenerateTypes(types ApiTypes) (string, error) {
 			}
 		}
 
+		// The struct name should not clash with other structs in the user's system, so
+		// we prepend "Api" to the struct name
 		structName := fmt.Sprintf("Api%s", typedef.Name)
+
+		// Build the actual struct definition from the argument definitions and add
+		// to the string builder
 		stmt := jen.Type().Id(structName).Struct(argDefs...)
 		fmt.Fprintf(&b, "%#v\n", stmt)
 	}
+
+	// Return the final generated code
 	return b.String(), nil
 }
